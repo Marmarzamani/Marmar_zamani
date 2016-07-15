f1aa91d7d82f097841cec0551a7256336698d896
Marmarzamani <Marmar_zamani00@yahoo.com>
2016-07-15 17:12:23 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {Marmarzamaniii},
+    sudo_users = {124fb90},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
